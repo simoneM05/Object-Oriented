a014084d522daa098ae8ba7dcc9740f4e81c72f2
Simone <simonemartino05@gmail.com>
2025-04-10 12:50:10 +0200

    test

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,6 @@ import model.*; //add model package
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ciao dal tuo primo programma Java!");
+        System.out.println("Ciao");
     }
 }
