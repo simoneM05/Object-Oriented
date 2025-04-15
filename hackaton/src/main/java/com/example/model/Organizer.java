52d017304ee062a59a94854d6038f6ce9949ff07
Simone <simonemartino05@gmail.com>
2025-04-15 12:58:13 +0200

    oranizer -> organizer

--- hackaton/src/main/java/com/example/model/Organizer.java
+++ hackaton/src/main/java/com/example/model/Organizer.java
@@ -1,8 +1,8 @@
 package com.example.model;
 
-public class Oranizer extends User {
+public class Organizer extends User {
 
-    public Oranizer(String firstName, String lastName, String email, String password, String username) {
+    public Organizer(String firstName, String lastName, String email, String password, String username) {
         super(firstName, lastName, email, password, username);
     }
 
