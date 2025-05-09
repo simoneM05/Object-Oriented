b2db66603a0b2fa553c160ccb101473dab696a51
Simone <simonemartino05@gmail.com>
2025-05-09 03:12:54 +0200

    use dev branch

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,14 +1,10 @@
 package com.example;
 
-import com.example.view.*;
-
 import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            LoginPage loginPage = new LoginPage();
-            loginPage.setVisible(true);
         });
     }
 }
