6b18d37d2dacab33332d1801ede52df717ec23fa
Simone <simonemartino05@gmail.com>
2025-05-22 14:47:59 +0200

    merge

--- src/main/java/com/example/model/Organizer.java
+++ src/main/java/com/example/model/Organizer.java
@@ -4,8 +4,9 @@ public class Organizer extends User {
 
     private String hackatonTitle; // hackaton a cui fa parte
     private boolean registrationStatus; // attributo per capire se le registrazione sono aperte o meno
+
     public Organizer(String firstName, String lastName, String email, Hackathon hackathon, String password,
-                     String username, boolean registrationStatus) /* throws CredenzialiNonValide */ {
+            String username, boolean registrationStatus) /* throws CredenzialiNonValide */ {
         super(firstName, lastName, email, password, username);
         this.hackatonTitle = hackathon.getTitle();
         this.registrationStatus = registrationStatus;
@@ -14,27 +15,28 @@ public class Organizer extends User {
     public void sendMailInvite(User user) {
         // todo: puo essere fatto solo a chi fa parte di quel hackaton
     }
-///
-//  public void chooseJugde(//la mia intenzione è quella di creare un arraylist di persone, su cui fare un foreach usando instanceof)
-//{
-//
-//  }
+    ///
+    // public void chooseJugde(//la mia intenzione è quella di creare un arraylist
+    /// di persone, su cui fare un foreach usando instanceof)
+    // {
+    //
+    // }
 
-    public void setRegistrationStatus(boolean registrationStatus) {
+    public void RegistrationStatus(boolean registrationStatus) {
         this.registrationStatus = registrationStatus;
     }
 
     public boolean isRegistrationAvaible(boolean registrationStatus) {
-        if (registrationStatus == true) {
+        if (registrationStatus == true) {c
             System.out.println("Registration is available.");
             return true;
-        }
-        else {
+        } else {
             System.out.println("Registration is not available.");
             return false;
         }
     }
 
-    //TODO = L'organizzatore deve aprire le registrazioni, in questa funzione richiamerà setRegistrationStatus
+    // TODO = L'organizzatore deve aprire le registrazioni, in questa funzione
+    // richiamerà setRegistrationStatus
 
 }
